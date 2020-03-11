fa4d0f8217ccbb56bbbcacf4cb20d7f3e27c7e28
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-11 15:22:24 -0300

    Add frases_do_dia

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,31 +1,83 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main(){
-  runApp (MaterialApp(
-    // debugShowCheckedModeBanner: false,
-    title: "Frases do dia",
-    // Container serve para colocar outros widgets nele
-    home: Container(
-      // color: Colors.white,
-      
-      // espaçamento interno
-      // padding: EdgeInsets.only(top: 40),
-      
-      // espaçamento externo
-      // margin: EdgeInsets.only(top: 50, left: 50),
-      margin: EdgeInsets.all(0),
-      
-      decoration: BoxDecoration(
-        border: Border.all(width: 3, color: Colors.white)
-      ),
-      
-      // child é filho só pode definir um único item
-      // children pode ser vários items
-      child: Image.asset(
-        "images/mesa.jpg",
-        fit: BoxFit.contain
-      ),
-    ),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home()
   ));
 }
 
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _frases = [
+    'O importante não é vencer todos os dias, mas lutar sempre.',
+    'É melhor conquistar a si mesmo do que vencer mil batalhas.',
+    'O medo de perder tira a vontade de ganhar.',
+    'Perder para a razão, sempre é ganhar.'
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase(){
+    var numeroSorteado = Random().nextInt(_frases.length);
+    // print(numeroSorteado);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia")  ,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          // Mesma coisa que o Center do Container
+          // width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.amber)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text (
+                _fraseGerada,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black
+                )
+              ),
+              RaisedButton(
+                onPressed: _gerarFrase,
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+                color: Colors.green
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
