41354e6c1dbada2444c0054b1a8738791968382e
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-11 17:44:07 -0300

    Terminando Alcool ou Gasolina

--- alcool_gasolina/lib/Home2.dart
+++ alcool_gasolina/lib/Home2.dart
@@ -1,39 +1,50 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
   String _textoResultado = "";
 
-  void _calcular(){
-    double precoAlcool = double.tryParse(_controllerAlcool.text);
-    double precoGasolina = double.tryParse(_controllerGasolina.text);
 
-    if (precoAlcool == null || precoGasolina == null) {
+  void _calcular(){
+
+    double precoAlcool = double.tryParse( _controllerAlcool.text );
+    double precoGasolina = double.tryParse( _controllerGasolina.text );
+
+    if( precoAlcool == null || precoGasolina == null ){
       setState(() {
-        _textoResultado = "Número inválido, digite núemros maiores que 0 e utilizando (.)!";  
+        _textoResultado = "Número inválido, digite números maiores que 0 e utilizando (.) ";
       });
     } else {
+
+      /*
+      * Se o preço do álcool divido pelo preço da gasolina
+      * for >= a 0.7 é melhor abastecer com gasolina
+      * senão é melhor utilizar álcool
+      * */
       if( (precoAlcool / precoGasolina) >= 0.7 ){
         setState(() {
           _textoResultado = "Melhor abastecer com gasolina";
         });
-      } else {
+      }else{
         setState(() {
-          _textoResultado = "Melhor abastecer com alcool";
+          _textoResultado = "Melhor abastecer com álcool";
         });
       }
+
+      //_limparCampos();
+
     }
-    _limparCampos();
+
+
   }
-  
+
   void _limparCampos(){
     _controllerGasolina.text = "";
     _controllerAlcool.text = "";
@@ -47,8 +58,8 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
         child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -59,63 +70,62 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: EdgeInsets.only(bottom: 10),
                 child: Text(
-                  "Saiba qual a emlhor opção para abastecimento do seu carro",
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
                   style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
                   ),
-                )
+                ),
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: "Preço Álcool, ex: 1.59"
+                    labelText: "Preço Alcool, ex: 1.59"
                 ),
                 style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.black
+                    fontSize: 22
                 ),
                 controller: _controllerAlcool,
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: "Preço Gasolina, ex: 1.59"
+                    labelText: "Preço Gasolina, ex: 3.59"
                 ),
                 style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.black
+                    fontSize: 22,
+                    color: Colors.red
                 ),
                 controller: _controllerGasolina,
               ),
               Padding(
                 padding: EdgeInsets.only(top: 10),
                 child: RaisedButton(
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  padding: EdgeInsets.all(15),
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(
-                      fontSize: 20
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
                     ),
-                  ),
-                  onPressed: _calcular,
-                )
+                    onPressed: _calcular
+                ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
                   _textoResultado,
                   style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold
-                  )
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               )
             ],
-          )
-        )
+          ),
+        ),
       ),
     );
   }
