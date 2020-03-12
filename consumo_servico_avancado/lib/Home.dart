bcf8824057fca116ac887dabd3a1fe9d727badb0
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-12 17:49:57 -0300

    Carregando dados em lista

--- consumo_servico_avancado/lib/Home.dart
+++ consumo_servico_avancado/lib/Home.dart
@@ -1,58 +1,81 @@
+import 'package:consumo_servico_avancado/Post.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
 class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
-  Future<Map> _recuperarPreco() async{
-    String url = "https://blockchain.info/ticker";
-    
-    http.Response response = await http.get(url);
+  String _urlBase = "https://jsonplaceholder.typicode.com";
 
-    return json.decode(response.body);
+  Future<List<Post>> _recuperarPostagens() async {
+    
+    http.Response response = await http.get( _urlBase + "/posts" );
+    var dadosJson = json.decode( response.body );
+
+    List<Post> postagens = List();
+    for( var post in dadosJson ){
+      
+      print("post: " + post["title"] );
+      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+      postagens.add( p );
+    
+    }
+    return postagens;
+    //print( postagens.toString() );
+    
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: _recuperarPreco(),
-      builder: (context, snapshot){
-        String resultado;
-        switch(snapshot.connectionState){
-          case ConnectionState.none:
-            print("conexão none");
-            break;
-          case ConnectionState.waiting:
-            resultado = "Carregando...";
-            print("conexão waiting");
-            break;
-          case ConnectionState.active:
-            print("conexão active");
-            break;
-          case ConnectionState.done:
-            print("conexão done");
-            if (snapshot.hasError)
-              resultado = "Erro ao carregar os dados";
-            else {
-              double valor = snapshot.data["BRL"]["buy"];
-              resultado = valor.toString();
-            }
-            break;            
-          default:
-            break;
-        }
-        return Center(
-          child: Text(resultado),
-        );
-      }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviço avançado"),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: _recuperarPostagens(),
+        builder: (context, snapshot){
+
+          switch( snapshot.connectionState ){
+            case ConnectionState.none :
+            case ConnectionState.waiting :
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+              break;
+            case ConnectionState.active :
+            case ConnectionState.done :
+              if( snapshot.hasError ){
+                print("lista: Erro ao carregar ");
+              }else {
+
+                print("lista: carregou!! ");
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                    itemBuilder: (context, index){
+
+                      List<Post> lista = snapshot.data;
+                      Post post = lista[index];
+
+                      return ListTile(
+                        title: Text( post.title ),
+                        subtitle: Text( post.id.toString() ),
+                      );
+                    
+                    }
+                );
+
+              }
+              break;
+          }
+
+        },
+      ),
     );
   }
 }
