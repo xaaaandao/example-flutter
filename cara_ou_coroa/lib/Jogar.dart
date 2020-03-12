e7e33dc9ca2b75631c64226e115fe6ae3b799405
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-12 10:54:02 -0300

    add interface cara ou coroa

---
+++ cara_ou_coroa/lib/Jogar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  Jogar({Key key}) : super(key: key);
+
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Color(0xffffcc80),
+      // backgroundColor: Color.fromRGBO(255, 204, 128, 1),
+      // backgroundColor: Colors.green,
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png")
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
---
+++ cara_ou_coroa/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'Jogar.dart';
+
+void main() => runApp(MaterialApp(
+  home: Jogar(),
+  debugShowCheckedModeBanner: false,
+));
+
