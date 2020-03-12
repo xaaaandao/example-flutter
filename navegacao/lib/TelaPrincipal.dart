3a37d0834e0ba0146c913b5abcba7e2bbd13f31d
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-12 09:37:01 -0300

    Add exemplo de navegacao

---
+++ navegacao/lib/TelaPrincipal.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'TelaSecundaria.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  TelaPrincipal({Key key}) : super(key: key);
+
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Ir para a segunda tela"),
+              padding: EdgeInsets.all(15),
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TelaSecundaria()
+                  )
+                );
+              },
+            )
+          ],
+        )
+      )
+    );
+  }
+}
---
+++ navegacao/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'TelaPrincipal.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
