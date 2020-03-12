bb589dfb922116cab0f4ecde523ba763e48ff300
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-12 08:48:49 -0300

    Add exemplos de radio button

--- entrada_dados/lib/main.dart
+++ entrada_dados/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'CampoTexto.dart';
 import 'EntradaCheckBox.dart';
+import 'EntradaRadioButton.dart';
 
 void main(){
   runApp(
     MaterialApp(
       // home: CampoTexto(),
-      home: EntradaCheckBox(),
+      // home: EntradaCheckBox(),
+      home: EntradaRadioButton(),
     )
   );
 }
