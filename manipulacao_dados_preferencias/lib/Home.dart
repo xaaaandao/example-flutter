ed026ab1e336f271afd65f21204792fdc2f3520b
Alexandre / Desenvolvimento Official TI <desenv01@officialti.com.br>
2020-03-13 16:44:45 -0300

    Iniciando a manipulação de dados

---
+++ manipulacao_dados_preferencias/lib/Home.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerCampo = TextEditingController();
+
+  _salvar(){
+
+  }
+
+  _recuperar(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Manipulação de dados"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Nada salvo!",
+              style: TextStyle(
+                fontSize: 20
+              )
+            ),
+            TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Digite algo"
+              ),
+              controller: _controllerCampo,
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(
+                      fontSize: 20
+                    )
+                  ),
+                  onPressed: _salvar,
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "Recuperar",
+                    style: TextStyle(
+                      fontSize: 20
+                    )
+                  ),
+                  onPressed: _recuperar,
+                )
+              ],
+            )
+          ],
+        )
+      )
+    );
+  }
+}
