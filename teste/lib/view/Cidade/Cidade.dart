7e1a51382ee109e0a50b64a1db54ab9cf763aa46
Alexandre Kajihara <alexandre.ykz@gmail.com>
2020-06-26 11:17:30 -0300

    add tabela cidades

--- teste/lib/view/Cidade/Cidade.dart
+++ teste/lib/view/Cidade/Cidade.dart
@@ -24,84 +24,79 @@ class _CidadeState extends State<Cidade> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getMyAppBar2(),
-      /*
-      body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 3,
-          // Generate 100 widgets that display their index in the List.
-          children: List.generate(100, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            );
-          }),
-        ),
-        */
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Table(
-//          defaultColumnWidth:
-//              FixedColumnWidth(MediaQuery.of(context).size.width / 3),
           border: TableBorder.all(
-              color: Colors.black26, width: 1, style: BorderStyle.none),
+          color: Colors.black26, width: 1, style: BorderStyle.solid),
           children: [
-            TableRow(children: [
-              TableCell(child: Center(child: Text('Title'))),
-              TableCell(
-                child: Center(child: Text('Title')),
-              ),
-              TableCell(child: Center(child: Text('Title'))),
-              TableCell(child: Center(child: Text('Title'))),
-            ]),
-            TableRow(children: [
-              TableCell(
-                child: Center(child: Text('Value')),
-                verticalAlignment: TableCellVerticalAlignment.bottom,
-              ),
-              TableCell(
-                verticalAlignment: TableCellVerticalAlignment.middle,
-                child: Center(child: Text('Value')),
-              ),
-              TableCell(
-                  child: Center(
-                      child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    border: Border.all(color: Colors.black26),
-                    borderRadius: BorderRadius.all(Radius.circular(16))),
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'ValueaaValueaaValueaaValueaaValueaaValueaaValueaa',
-                  style: TextStyle(color: Colors.yellow),
-                ),
-              ))),
-              TableCell(
-                child: Center(child: Text('Value')),
-                verticalAlignment: TableCellVerticalAlignment.top,
-              ),
-            ]),
-            TableRow(children: [
-              TableCell(child: Center(child: Text('Value'))),
-              TableCell(
-                child: Center(child: Text('Value')),
-              ),
-              TableCell(child: Center(child: Text('Value'))),
-              TableCell(child: Center(child: Text('Value'))),
-            ]),
-            TableRow(children: [
-              TableCell(child: Center(child: Text('Value'))),
-              TableCell(
-                child: Center(child: Text('Value')),
-              ),
-              TableCell(child: Center(child: Text('Value'))),
-              TableCell(child: Center(child: Text('Value'))),
-            ])
+            defineCabecalhoLinha(),
+            defineValorLinha("valorUm", "valorDois"),
+            defineValorLinha("valorTres", "valorQuatro"),
           ],
         ),
       ),        
     );
+  }
+
+  TableRow defineCabecalhoLinha(){
+    return new TableRow(
+                children: [
+                  defineCelulaTabela("Cidade"),
+                  defineCelulaTabela("UF"),
+                  defineCelulaTabela("Editar/Remover")
+                ]
+              );
+  }
+
+  TableRow defineValorLinha(valorUm, valorDois){
+    return new TableRow(
+                children: [
+                  defineCelulaTabela(valorUm),
+                  defineCelulaTabela(valorDois),
+                  defineCelulaEditaRemover()
+                ],
+              );
+  }
+/*
+  Icon(
+                        Icons.edit,
+                        color: Colors.green,
+                        size: 30.0,
+                      ),
+                      Icon(
+                        Icons.restore_from_trash,
+                        color: Colors.red,
+                        size: 30.0,
+                      ),
+*/
+  TableCell defineCelulaEditaRemover(){
+    return new TableCell(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
+                  children: <Widget>[
+                    Icon(
+                      Icons.edit,
+                      color: Colors.green,
+                      size: 30.0,
+                    ),
+                    Icon(
+                      Icons.restore_from_trash,
+                      color: Colors.red,
+                      size: 30.0,
+                    ),
+                  ],
+                )
+              );
+  }
+
+  TableCell defineCelulaTabela(text){
+    return new TableCell(
+                child: Center(
+                  child: getMyText(text, 20)
+                ),
+                verticalAlignment: TableCellVerticalAlignment.middle,
+              );
   }
 }
